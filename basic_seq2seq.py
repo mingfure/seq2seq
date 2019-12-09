ea4cbdb8ee1226789435d7757ccfb8195fe6d962
mingfure <mingfure@163.com>
2019-12-09 11:01:32 +0800

    modified target format

--- basic_seq2seq.py
+++ basic_seq2seq.py
@@ -7,7 +7,7 @@ import time
 
 # 超参数
 # Number of Epochs
-epochs = 1000
+epochs = 100
 # Batch Size
 batch_size = 16 #50
 # RNN Size
@@ -23,16 +23,6 @@ learning_rate = 0.001
 # seq max length: 25
 ###
 
-# source_data,target_data = bytes(),bytes()
-with open('data/source.txt','r',encoding='utf-8') as f:
-    source_data = f.read()
-
-with open('data/target.txt','r',encoding='utf-8') as f:
-    target_data = f.read()
-
-print(source_data.split('\n')[0])
-print(target_data.split('\n')[0])
-
 # source line：[[w1],[w3,w2,w4]]\n
 def extract_source_vocab(data):
     """
@@ -42,7 +32,7 @@ def extract_source_vocab(data):
     special_words = ['<PAD>','<UNK>','<GO>','<EOS>']
 
     set_words = list(set([word for line in data.split('\n') for seq in eval(line) for word in seq]))
-    print("set_words nums", len(set_words))
+    # print("set_words nums", len(set_words))
     int_to_vocab = {idx:word for idx,word in enumerate(special_words + set_words)}
     vocab_to_int = {word:idx for idx,word in int_to_vocab.items()}
 
@@ -55,35 +45,12 @@ def  extract_target_vocab(data):
     :return: target字符映射表
     """
     special_words = ['<PAD>','<UNK>','<GO>','<EOS>']
-    set_words = list([word for word in data.split('\n')])
-    print("target num", len(set_words))
+    set_words = list(set([word for word in data.split('\n')]))
+    print("Dataset Size:", len(set_words))
     int_to_vocab = {idx:word for idx,word in enumerate(special_words + set_words)}
     vocab_to_int = {word:idx for idx,word in int_to_vocab.items()}
 
     return int_to_vocab,vocab_to_int    
-
-# 得到输入和输出的字符映射表 word <--> int
-source_int_to_word,source_word_to_int = extract_source_vocab(source_data)
-target_int_to_word,target_word_to_int = extract_target_vocab(target_data)
-
-# 将每一行转换成word id的list(list())
-source_int = list()
-for line in source_data.split('\n'):
-    line_int = list()
-    for seq in eval(line):
-        seq_int = list()
-        for word in seq:
-            seq_int.append(source_word_to_int.get(word,source_word_to_int['<UNK>']))
-        line_int.append(seq_int)
-    source_int.append(line_int)
-
-# source_int = [[source_word_to_int.get(word, source_word_to_int['<UNK>']) for seq in line for word in seq] for line in source_data.split('\n')]
-
-target_int = [target_word_to_int.get(word, target_word_to_int['<UNK>']) for word in target_data.split('\n')]
-# target_int = [target_word_to_int.get(word, target_word_to_int['<UNK>']) for word in target_data.split('\n')] + [target_word_to_int['<EOS>']] 
-
-print("source", source_int[0])
-print("target", target_int[0])
 
 # 输入层
 def get_inputs():
@@ -93,12 +60,12 @@ def get_inputs():
 
     # 定义target序列最大长度（之后target_sequence_length和source_sequence_length会作为feed_dict的参数）
     target_sequence_length = tf.placeholder(tf.int32,(None,),name='target_sequence_length')
-    print("target_sequence_length",target_sequence_length)
+    # print("target_sequence_length",target_sequence_length)
 
     max_target_sequence_length = tf.reduce_max(target_sequence_length,name='max_target_len')
-    print("max_target_sequence_length",max_target_sequence_length)
+    # print("max_target_sequence_length",max_target_sequence_length)
     source_sequence_length = tf.placeholder(tf.int32,(None,),name='source_sequence_length')
-    print("source_sequence_length",source_sequence_length)
+    # print("source_sequence_length",source_sequence_length)
 
 
     return inputs,targets,learning_rate,target_sequence_length,max_target_sequence_length,source_sequence_length
@@ -227,6 +194,7 @@ def decoding_layer(target_word_to_int,decoding_embedding_size,num_layers,rnn_siz
 
     return training_decoder_output,predicting_decoder_output
 
+
 # 上面已经构建完成Encoder和Decoder，下面将这两部分连接起来，构建seq2seq模型
 def seq2seq_model(input_data,targets,lr,target_sequence_length,max_target_sequence_length,
                   source_sequence_length,source_vocab_size,target_vocab_size,encoder_embedding_size,
@@ -253,9 +221,71 @@ def seq2seq_model(input_data,targets,lr,target_sequence_length,max_target_sequen
     return training_decoder_output,predicting_decoder_output
 
 
+def pad_sentence_batch(sentence_batch,pad_int): # 需不需要设置为定长? eg. 60
+    '''
+    对batch中的序列进行补全，保证batch中的每行都有相同的sequence_length
+
+    参数：
+    - sentence batch
+    - pad_int: <PAD>对应索引号
+    '''
+    max_sentence = max([len(sentence) for sentence in sentence_batch])
+    return [sentence + [pad_int] * (max_sentence - len(sentence)) for sentence in sentence_batch]
 
 
+def get_batches(targets,sources,batch_size,source_pad_int,target_pad_int):
 
+    for batch_i in range(0,len(sources)//batch_size):
+        start_i = batch_i * batch_size
+        sources_batch = sources[start_i : start_i + batch_size]
+        targets_batch = targets[start_i : start_i + batch_size]
+
+        # print('source_batch',sources_batch)
+        # print("target_batch",targets_batch)
+        
+        pad_sources_batch = np.array(pad_sentence_batch(sources_batch,source_pad_int))
+        pad_targets_batch = np.array(pad_sentence_batch(targets_batch,target_pad_int))
+
+        targets_lengths = []
+        for target in targets_batch:
+            targets_lengths.append(len(target))
+
+        source_lengths = []
+        for source in sources_batch:
+            source_lengths.append(len(source))
+        yield pad_targets_batch,pad_sources_batch,targets_lengths,source_lengths
+
+
+def source_to_seq(text):
+    sequence_length = len(text)
+    return [source_word_to_int.get(word,source_word_to_int['<UNK>']) for word in text] + [source_word_to_int['<PAD>']] * (sequence_length - len(text))
+
+
+### 执行过程 ###
+with open('data/source.txt','r',encoding='utf-8') as f:
+    source_data = f.read()
+
+with open('data/target.txt','r',encoding='utf-8') as f:
+    target_data = f.read()
+
+# 得到输入和输出的字符映射表 word <--> int
+source_int_to_word,source_word_to_int = extract_source_vocab(source_data)
+target_int_to_word,target_word_to_int = extract_target_vocab(target_data)
+
+# 将每一行转换成word id的list(list())
+source_int = list()
+for line in source_data.split('\n'):
+    line_int = list()
+    for seq in eval(line):
+        seq_int = list()
+        for word in seq:
+            seq_int.append(source_word_to_int.get(word,source_word_to_int['<UNK>']))
+        line_int.append(seq_int)
+    source_int.append(line_int)
+target_int = [target_word_to_int.get(word, target_word_to_int['<UNK>']) for word in target_data.split('\n')]
+
+# print("source", source_int[0])
+# print("target", target_int[0])
 
 # 构造graph --> 为啥要构造graph???
 train_graph = tf.Graph()
@@ -313,52 +343,17 @@ with train_graph.as_default():
         # 将计算出的梯度应用到变量上，是函数minimize()的第二部分，返回一个应用指定的梯度的操作Operation，对global_step做自增操作
         train_op = optimizer.apply_gradients(capped_gradients)
 
-
-def pad_sentence_batch(sentence_batch,pad_int): # 需不需要设置为定长? eg. 60
-    '''
-    对batch中的序列进行补全，保证batch中的每行都有相同的sequence_length
-
-    参数：
-    - sentence batch
-    - pad_int: <PAD>对应索引号
-    '''
-    max_sentence = 30 # max([len(sentence) for sentence in sentence_batch])
-    return [sentence + [pad_int] * (max_sentence - len(sentence)) for sentence in sentence_batch]
-
-
-def get_batches(targets,sources,batch_size,source_pad_int,target_pad_int):
-
-    for batch_i in range(0,len(sources)//batch_size):
-        start_i = batch_i * batch_size
-        sources_batch = sources[start_i : start_i + batch_size]
-        targets_batch = targets[start_i : start_i + batch_size]
-
-        print('source_batch',sources_batch)
-        print("target_batch",targets_batch)
-        
-        pad_sources_batch = np.array(pad_sentence_batch(sources_batch,source_pad_int))
-        pad_targets_batch = np.array(pad_sentence_batch(targets_batch,target_pad_int))
-
-        targets_lengths = []
-        for target in targets_batch:
-            targets_lengths.append(len(target))
-
-        source_lengths = []
-        for source in sources_batch:
-            source_lengths.append(len(source))
-        yield pad_targets_batch,pad_sources_batch,targets_lengths,source_lengths
-
-
-
 # Train
 # 如果我们只看第一个序列的话，那就是标准的seq了
+
 source_tmp = list()
 target_tmp = list()
 for s in source_int:
     source_tmp.append(s[-1])
 for t in target_int:
     target_tmp.append([t])
-# print(target_tmp)
+# print("source_tmp",source_tmp)
+# print("target_tmp",target_tmp)
 
 train_source = source_tmp[batch_size:]
 train_target = target_tmp[batch_size:]
@@ -366,11 +361,12 @@ train_target = target_tmp[batch_size:]
 # 留出一个batch进行验证
 valid_source = source_tmp[:batch_size]
 valid_target = target_tmp[:batch_size]
-# print("valid_target",valid_target)
+print("valid_source: ",valid_source)
+print("valid_target: ",valid_target)
 
 (valid_targets_batch, valid_sources_batch, valid_targets_lengths, valid_sources_lengths) = next(get_batches(valid_target, valid_source, batch_size,
-                           source_word_to_int['<PAD>'],
-                           target_word_to_int['<PAD>']))
+                        source_word_to_int['<PAD>'],
+                        target_word_to_int['<PAD>']))
 
 display_step = 50
 
@@ -382,7 +378,7 @@ with tf.Session(graph=train_graph) as sess:
     for epoch_i in range(1,epochs+1):
         for batch_i,(targets_batch, sources_batch, targets_lengths, sources_lengths) in enumerate(get_batches(
             train_target,train_source,batch_size,source_word_to_int['<PAD>'],
-                           target_word_to_int['<PAD>']
+                        target_word_to_int['<PAD>']
         )):
             _,loss = sess.run([train_op,cost],feed_dict={
                 input_data:sources_batch,
@@ -397,58 +393,57 @@ with tf.Session(graph=train_graph) as sess:
                 validation_loss = sess.run(
                     [cost],
                     {input_data: valid_sources_batch,
-                     targets: valid_targets_batch,
-                     lr: learning_rate,
-                     target_sequence_length: valid_targets_lengths,
-                     source_sequence_length: valid_sources_lengths})
+                    targets: valid_targets_batch,
+                    lr: learning_rate,
+                    target_sequence_length: valid_targets_lengths,
+                    source_sequence_length: valid_sources_lengths})
 
                 print('Epoch {:>3}/{} Batch {:>4}/{} - Training Loss: {:>6.3f}  - Validation loss: {:>6.3f}'
-                      .format(epoch_i,
-                              epochs,
-                              batch_i,
-                              len(train_source) // batch_size,
-                              loss,
-                              validation_loss[0]))
+                    .format(epoch_i,
+                            epochs,
+                            batch_i,
+                            len(train_source) // batch_size,
+                            loss,
+                            validation_loss[0]))
 
     saver = tf.train.Saver()
     saver.save(sess, checkpoint)
     print('Model Trained and Saved')
 
 
+
 # 预测
-def source_to_seq(text):
-    sequence_length = 30
-    return [source_word_to_int.get(word,source_word_to_int['<UNK>']) for word in text] + [source_word_to_int['<PAD>']] * (sequence_length - len(text))
 
+# def do_one_predict():
+    source_line = ['abort', '__stack_chk_fail']
+    text = source_to_seq(source_line)
+    # print("text:",text)
 
-source_line = "['abort', '__stack_chk_fail']"
-text = source_to_seq(source_line)
+    checkpoint = "model/trained_model.ckpt"
+    loaded_graph = tf.Graph()
 
-checkpoint = "model/trained_model.ckpt"
-loaded_graph = tf.Graph()
+    with tf.Session(graph=loaded_graph) as sess:
+        loader = tf.train.import_meta_graph(checkpoint+'.meta')
+        loader.restore(sess,checkpoint)
 
-with tf.Session(graph=loaded_graph) as sess:
-    loader = tf.train.import_meta_graph(checkpoint+'.meta')
-    loader.restore(sess,checkpoint)
+        input_data = loaded_graph.get_tensor_by_name('inputs:0')
+        logits = loaded_graph.get_tensor_by_name('predictions:0')
+        source_sequence_length = loaded_graph.get_tensor_by_name('source_sequence_length:0')
+        target_sequence_length = loaded_graph.get_tensor_by_name('target_sequence_length:0')
 
-    input_data = loaded_graph.get_tensor_by_name('inputs:0')
-    logits = loaded_graph.get_tensor_by_name('predictions:0')
-    source_sequence_length = loaded_graph.get_tensor_by_name('source_sequence_length:0')
-    target_sequence_length = loaded_graph.get_tensor_by_name('target_sequence_length:0')
+        # print("[len(source_line)] * batch_size",[len(source_line)] * batch_size)
+        answer_logits = sess.run(logits, {input_data: [text] * batch_size,
+                                        target_sequence_length: [1] * batch_size, # 1:len(source_line)
+                                        source_sequence_length: [len(source_line)] * batch_size})[0]
 
-    print("[len(source_line)] * batch_size",[len(source_line)] * batch_size)
-    answer_logits = sess.run(logits, {input_data: [text] * batch_size,
-                                      target_sequence_length: [len(source_line)] * batch_size,
-                                      source_sequence_length: [len(source_line)] * batch_size})[0]
+        pad = source_word_to_int["<PAD>"]
 
-    pad = source_word_to_int["<PAD>"]
+        print('原始输入:', source_line)
 
-    print('原始输入:', source_line)
+        print('\nSource')
+        print('  Word 编号:    {}'.format([i for i in text]))
+        print('  Input Words: {}'.format(" ".join([source_int_to_word[i] for i in text])))
 
-    print('\nSource')
-    print('  Word 编号:    {}'.format([i for i in text]))
-    print('  Input Words: {}'.format(" ".join([source_int_to_word[i] for i in text])))
-
-    print('\nTarget')
-    print('  Word 编号:       {}'.format([i for i in answer_logits if i != pad]))
-    print('  Response Words: {}'.format(" ".join([target_int_to_word[i] for i in answer_logits if i != pad])))
+        print('\nTarget')
+        print('  Word 编号:       {}'.format([i for i in answer_logits if i != pad]))
+        print('  Response Words: {}'.format(" ".join([target_int_to_word[i] for i in answer_logits if i != pad])))
